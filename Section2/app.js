3495c0bcb341a3a60e76cc726200130a034ceae5
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-06-04 03:07:09 +0530

    Basic types Completed

--- Section2/app.js
+++ Section2/app.js
@@ -355,7 +355,7 @@ var tester = {
     },
     hobbies: [Hobbies.DANCING, Hobbies.SINGING],
     role: Role.ADMIN,
-    favouriteActivity: FavouriteActivities.DRUGS
+    favouriteActivity: FavouriteActivities.DRUGS,
 };
 //console the whole object
 console.log(tester);
@@ -367,12 +367,387 @@ console.log(tester.hobbies[1]);
 console.log(tester.favouriteActivity);
 //can be used in comparison
 if (tester.role === Role.ADMIN) {
-    console.log('this tester is an ADMIN');
+    console.log("this tester is an ADMIN");
 }
 //values of enum can be used for direct comparison
 if (tester.role === 0) {
-    console.log('this tester is an ADMIN');
+    console.log("this tester is an ADMIN");
 }
 //printed as an object which has constant: value and value:constant as properties
 console.log(Role);
 //-------------------------------Enums ends-----------------------------
+//-------------------------------Any-----------------------------
+console.log("");
+console.log("-------------------------------Any-----------------------------");
+//declare a variable of any type
+var favouriteTask;
+//declare an array of any type
+var dailyTasks;
+//set string value in any type of variable
+favouriteTask = "running";
+console.log(favouriteTask);
+//set number value in any type of variable
+favouriteTask = 22;
+console.log(favouriteTask);
+//set boolean value in any type of variable
+favouriteTask = true;
+console.log(favouriteTask);
+//set array value in any type of variable
+favouriteTask = ["coding", "developing"];
+console.log(favouriteTask);
+//set mix type array value in any type of variable
+favouriteTask = ["coding", 22];
+console.log(favouriteTask);
+//set string array value in any array type of variable
+dailyTasks = ["wake up", "eat", "code", "sleep", "repeat"];
+console.log(dailyTasks);
+//below line would give an error as ts expects it to be an array
+//dailyTasks='sleep';
+//set mix data type array value in any array type of variable
+dailyTasks = ["eat", "sleep", 22, "33", true];
+console.log(dailyTasks);
+//-------------------------------Any Ends-----------------------------
+//-------------------------------Union-----------------------------
+console.log("");
+console.log("//-------------------------------Union-----------------------------");
+//declare method params as union type
+function combine(input1, input2) {
+    //the input validation given below should not be required in all cases
+    //it depends on the logic of the method
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+//call with two strings
+var combineNames = combine("rudhra ", "koul");
+console.log(combineNames);
+//call with two numbers
+var combineAge = combine(22, 8);
+console.log(combineAge);
+var combineNameAndAge = combine("rudhra", 30);
+console.log(combineNameAndAge);
+//below line will give an error as  boolean cant be used only string and number can be
+//const combineTest=combine('rudhra',true);
+//union type variable
+var abc;
+abc = "heya";
+console.log(abc);
+abc = 55;
+console.log(abc);
+//below line will give an error as  boolean cant be used only string and number can be
+//abc=true;
+//-------------------------------Union Ends-----------------------------
+//-------------------------------Literal Types-----------------------------
+console.log("");
+console.log("-------------------------------Literal Types-----------------------------");
+//result type declared as literal type
+function combineIt(input1, input2, resultType) {
+    //the input validation given below should not be required in all cases
+    //it depends on the logic of the method
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
+        //Now since this condition can also be satisfied
+        // even if input 1 or input2 is not a number
+        //we havo explicitly convert it to numbers
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+//call with two strings and return result as a string
+var combineNames1 = combineIt("rudhra ", "koul", "as-string");
+console.log(combineNames1);
+//call with two numbers and return result as a number
+var combineAge1 = combineIt(22, 8, "as-number");
+console.log(combineAge1);
+//call with two strings and return result as a number
+var combineAge2 = combineIt("22", "8", "as-number");
+console.log(combineAge2);
+//call with two numbers and return result as a string
+var combineAge3 = combineIt(22, 8, "as-string");
+console.log(combineAge3);
+//call with a string and a number and return result as a string
+var combineNameAndAge1 = combineIt("rudhra", 30, "as-string");
+console.log(combineNameAndAge1);
+//following codee will result in NaN(Not A Number)
+//as 'Rudhra' cant be converted to a number
+var combineNameAndAge2 = combineIt("rudhra", 30, "as-number");
+console.log(combineNameAndAge2);
+//literal type with multiple types as allowed values
+function printRoleOrRoleId(xyz) {
+    console.log(xyz);
+}
+//call with allowed values
+printRoleOrRoleId("Manager");
+printRoleOrRoleId(1);
+printRoleOrRoleId("developer");
+printRoleOrRoleId(true);
+//following lines will give an error
+// as the value passed does not lie in allowed vaues for literal type xyz
+//printRoleOrRoleId('Engineer');
+//printRoleOrRoleId(2);
+//printRoleOrRoleId(false);
+//variable declared as a literal type
+var pqr;
+//assign allowed values to variable pqr
+pqr = "Manager";
+console.log(pqr);
+pqr = 1;
+console.log(pqr);
+pqr = "developer";
+console.log(pqr);
+pqr = true;
+console.log(pqr);
+//following lines will give an error
+// as the value assigned does not lie in allowed vaues for literal type pqr
+//pqr='Engineer';
+//pqr=2;
+//pqr=false;
+//-------------------------------Literal Types End-----------------------------
+//-------------------------------Types Aliases-----------------------------
+console.log("");
+console.log("-------------------------------Types Aliases-----------------------------");
+function combineItWithAlias(input1, input2, resultType) {
+    //the input validation given below should not be required in all cases
+    //it depends on the logic of the method
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
+        //Now since this condition can also be satisfied
+        // even if input 1 or input2 is not a number
+        //we havo explicitly convert it to numbers
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+//call with two strings and return result as a string
+var combineNames2 = combineItWithAlias("rudhra ", "koul", "as-string");
+console.log(combineNames2);
+//call with two numbers and return result as a number
+var combineAge22 = combineItWithAlias(22, 8, "as-number");
+console.log(combineAge22);
+//call with two strings and return result as a number
+var combineAge23 = combineItWithAlias("22", "8", "as-number");
+console.log(combineAge23);
+//call with two numbers and return result as a string
+var combineAge34 = combineItWithAlias(22, 8, "as-string");
+console.log(combineAge34);
+//call with a string and a number and return result as a string
+var combineNameAndAge11 = combineItWithAlias("rudhra", 30, "as-string");
+console.log(combineNameAndAge11);
+//following codee will result in NaN(Not A Number)
+//as 'Rudhra' cant be converted to a number
+var combineNameAndAge21 = combineItWithAlias("rudhra", 30, "as-number");
+console.log(combineNameAndAge21);
+//constant with type as a type alias
+var u1 = { name: "Rudhra", age: 30 };
+//following two functions can be simplified using type aliases for objects
+function greet(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
+function greet1(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder1(user, checkAge) {
+    return checkAge > user.age;
+}
+//call methods with params having type aliases
+greet1(u1);
+console.log(isOlder1(u1, 23));
+console.log(isOlder1(u1, 33));
+//assign a object to variable of type User1
+var u3 = { name: "Rudhra" };
+console.log(u3);
+//assign a string to a variable of User1 type
+u3 = "Hansa";
+console.log(u3);
+//Below code will give an error as boolean is not assignable to  the type User1
+//only object and string is permitted
+//u3=true;
+//below code will give an error as property age is unknown
+//to the type of obect assignable to u3
+//u3 ={name:"Chetna",age:567};
+//-------------------------------Types Aliases Ends-----------------------------
+//-------------------------------Functions-----------------------------
+console.log("");
+console.log("-------------------------------Functions-----------------------------");
+//no explicit return type and no parameteres
+function greetUser() {
+    console.log("heyaaaa");
+}
+//TS infers return type as number
+function addAgain(num1, num2) {
+    return num1 + num2;
+}
+//TS infers return type as string
+function concat(txt1, txt2) {
+    return txt1 + txt2;
+}
+//Explicitly specifying return type as number
+function addIt(num1, num2) {
+    return num1 + num2;
+}
+//Explicitly specifying return type as string
+function concatIt(txt1, txt2) {
+    return txt1 + txt2;
+}
+//Ts infers return type as void
+function printSum(num1, num2) {
+    console.log(num1 + num2);
+}
+//Explicitly specifying return type as void
+function displaySum(num1, num2) {
+    console.log(num1 + num2);
+}
+//Explicitly specifying return type as undefined
+function concatinateIt(txt1, txt2) {
+    console.log(txt1 + txt2);
+    return;
+}
+var rrd;
+console.log(greetUser());
+console.log(addAgain(22, 33));
+console.log(concat("yeah", "bro"));
+console.log(addIt(55, 85));
+console.log(concatIt("heya", "mister"));
+console.log(printSum(55, 88));
+console.log(displaySum(43, 67));
+console.log(concatinateIt("heyyyyyyyy", "brother"));
+console.log(rrd);
+//-------------------------------Functions Ends -----------------------------
+//-------------------------------Function As Type -----------------------------
+console.log("");
+console.log("-------------------------------Function As Type-----------------------------");
+//simply saving a function to a var
+var addFunc = addIt;
+console.log(addFunc(22, 33));
+//explicitly specifying variable to be a function type
+var addFunc2;
+addFunc2 = addIt;
+console.log(addFunc2(22, 33));
+//re assigning another function to addFunc2
+addFunc2 = concatIt;
+console.log(addFunc2("bro", " yeah!!!"));
+addFunc2 = greet1;
+addFunc2 = concatIt;
+//but now below statementreturn Nan
+//we assigned a function that takes in two strings
+//js treats it as two any after compilation to js
+//we call using one number
+//so other param is undefined
+//undefined +num is nan
+console.log(addFunc2(11));
+//to print the function definition
+console.log(addFunc2.toString());
+//we can be more specific by specifying exact signature
+var adds;
+adds = addIt;
+console.log(adds(55, 63));
+//below lines will give a compilation error as there is a signature mismatch
+//adds = concatIt;
+//adds(55);;
+//assign function with no params and void retur type
+var greets;
+greets = greetUser;
+greets();
+//-------------------------------Function As Type Ends -----------------------------
+//-------------------------------Function Types and Callbacks ------------------------
+console.log("");
+console.log("-------------------------------Function Types and Callbacks ------------------------");
+//Function with a callback argument
+//which takes in a number as a parameter and doesnt return anything
+function addAndHandleResult(num1, num2, cb) {
+    var result = num1 + num2;
+    //using the callback inside the function
+    cb(result);
+}
+//calling the function and passing an anonymous function as an argument for callback
+addAndHandleResult(20, 30, function (result) {
+    console.log(result);
+});
+//we can pass a callback function with no argument as ts
+//treats the argument as optional by default
+addAndHandleResult(20, 30, function () {
+    console.log("result calculated");
+});
+addAndHandleResult(20, 30, function (result) {
+    //we can call math.sqrt here because since we defined the input to callback as a number
+    //ts is sure  result will be a number
+    console.log("Square root of result is" + Math.sqrt(result));
+});
+//below code will give an error as we have specified that the
+//callback will have only one argument
+//and we are passing two arguments
+/* addAndHandleResult(20, 30, (result,abc) => {
+  console.log("Square root of result is" + Math.sqrt(result));
+}); */
+addAndHandleResult(20, 30, function (result) {
+    console.log("Square root of result is" + Math.sqrt(result));
+    //we can return somethineven though we specified the return type as void
+    //because by specifying void we are establishing a contract that the return value
+    //if any will not be used and thus ts ignores the return value
+    return Math.sqrt(result);
+});
+//Function with a callback argument
+//which takes in a number as a parameter and doesnt return anything
+function addAndHandleResultAgain(num1, num2, cb) {
+    var result = num1 + num2;
+    //using the callback inside the function
+    console.log("result is " + result);
+    //ts gives a warg that the return should n be used as it is void
+    console.log(cb(result));
+}
+addAndHandleResultAgain(40, 30, function (result) {
+    console.log("Square root of result is" + Math.sqrt(result));
+    //we can return something even though we specified the return type as void
+    //because by specifying void we are establishing a contract that the return value
+    //if any will not be used and thus ts ignores the return value
+    //bwe can stll return a value and it can be used in the function
+    return Math.sqrt(result);
+});
+//-------------------------------Function Types and Callbacks  Ends------------------------
+//-------------------------------Unknown Type------------------------
+console.log("");
+console.log("-------------------------------Unknown Type ------------------------");
+//create an unknown type
+var userInput;
+var userName;
+var userInput2;
+userInput2 = "chetna";
+//assign anumber
+userInput = 22;
+userInput = "Rudhra";
+//below line gives an error as we cant save an unknown type to a fix type
+//without type check
+//userName=userInput;
+//for type any assignment to a fix type can be done
+userName = userInput2;
+//assignment to a fix type can be done after a type check for unknown types
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+//-------------------------------Unknown Type Ends------------------------
+//-------------------------------Never Type------------------------
+console.log("");
+console.log("-------------------------------Never Type------------------------");
+//function that throws an error and never returns back
+function throwError(errorMessage, code) {
+    throw { message: errorMessage, errorCode: code };
+}
+throwError("An error occured", 500);
+console.log("This never prints");
+//-------------------------------Never Type Ends------------------------
