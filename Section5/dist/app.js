5b48980f0ef89e8d741e1a6a1c09c3b4cd719c6d
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-06-17 03:35:48 +0530

    Classes and Interfaces Complete

--- Section5/dist/app.js
+++ Section5/dist/app.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Vaango anna");
 //--------------------------------Classes ------------------------------
 console.log("");
@@ -81,8 +82,9 @@ console.log("----------------------------Access Modifiers----------------------"
 //define a new class
 class BusinessUnit {
     //constructor that initializes name property
-    constructor(n) {
+    constructor(n, employees) {
         this.name = n;
+        this.employees = employees;
     }
     describe() {
         console.log("Welcome to " + this.name + " Business Unit");
@@ -110,7 +112,7 @@ class BusinessUnit {
     }
 }
 //initialize the BusinessUnit class using new keyword and constructor call
-const retailBu = new BusinessUnit("Retail");
+const retailBu = new BusinessUnit("Retail", []);
 console.log(retailBu);
 retailBu.describe();
 //can access public properties
@@ -231,4 +233,350 @@ console.log("sbuId is " + retailSBu.sbuId);
 //retailSBu.sbuId=22;
 retailSBu.describe();
 //-----------------------------Read only Properties Ends----------------
+//-----------------------------Inheritance----------------
+console.log("");
+console.log("-----------------------------Inheritance----------------");
+//class that extends another class
+class AccountingDepartment extends Project {
+}
+//parent class constructor is automatically called
+//we need to pass name argument as it is required by parent class constructor
+const accounting = new AccountingDepartment("Accounting");
+console.log(accounting);
+class ItDepartment extends Project {
+    constructor(name, admins) {
+        //calling constructor of base class should be first line
+        super(name);
+        this.admins = admins;
+    }
+}
+const itDepartment = new ItDepartment("It", ["hansaa"]);
+console.log(itDepartment);
+class AdminDepartment extends Project {
+    constructor(admins) {
+        //calling constructor of base class should be first line
+        super("Admin");
+        this.admins = admins;
+    }
+    //new methods added
+    addAdmin(admin) {
+        this.admins.push(admin);
+    }
+    displayAdmins() {
+        console.log(this.admins);
+    }
+}
+const AdminDept = new AdminDepartment(["Rudhra"]);
+console.log(AdminDept);
+AdminDept.addAdmin("karan");
+AdminDept.displayAdmins();
+AdminDept.describe();
+//-----------------------------Inheritance Ends----------------
+//-----------------------------Overriding Properties & The "protected" Modifier----------------
+console.log("");
+console.log("-------------Overriding Properties & The protected Modifier-----------");
+class Floor {
+    constructor(floorName, floorMarshal, employees) {
+        this.floorName = floorName;
+        this.floorMarshal = floorMarshal;
+        this.employees = employees;
+    }
+    addEmployees(employee) {
+        this.employees.push(employee);
+    }
+    printEmployees() {
+        console.log("the employees on the floor are " + this.employees);
+    }
+    validateAndAddEmployees(employee) {
+        if (employee) {
+            this.employees.push(employee);
+        }
+    }
+    currentFloorName() {
+        return this.floorName;
+    }
+}
+class AdminFloor extends Floor {
+    constructor(floorMarshal, employees) {
+        super("FirstFloor", floorMarshal, employees);
+        this.floorMarshal = floorMarshal;
+        this.employees = employees;
+    }
+    /*
+    Cant override a private method
+    private addEmployees(employee: string) {
+      this.employees.push(employee);
+    } */
+    /*
+    cant access private properties
+    public printEmployees() {
+      console.log("the employees on of "+this.floorName +' are ' +this.employees);
+    } */
+    //can override public methods
+    printEmployees() {
+        //can access protected methods
+        console.log("the employees on of " +
+            this.currentFloorName() +
+            " are " +
+            this.employees);
+    }
+    //can override protected method
+    validateAndAddEmployees(employee) {
+        if (employee !== "") {
+            //can access protected properties
+            this.employees.push(employee);
+        }
+    }
+    //override public method
+    currentFloorName() {
+        //can access protected method
+        return super.currentFloorName();
+    }
+}
+const adminFloor = new AdminFloor("Neeraj", ["chetna", "namit"]);
+console.log(adminFloor);
+console.log(adminFloor.currentFloorName());
+//cant access protected members from outside the class
+//adminFloor.validateAndAddEmployees('shobhit');
+//-----------------------------Overriding Properties & The "protected" Modifier Ends----------------
+//----------------------------- Getters & Setters----------------
+console.log("");
+console.log("------------- Getters & Setters-----------");
+class AccountingFloor extends Floor {
+    constructor(floorMarshal, employees, reports) {
+        super("FirstFloor", floorMarshal, employees);
+        this.floorMarshal = floorMarshal;
+        this.employees = employees;
+        this.reports = reports;
+        this.currentReport = "";
+    }
+    //getter
+    get recentReport() {
+        if (this.currentReport) {
+            return this.currentReport;
+        }
+        else if (this.reports[0]) {
+            return this.reports[0];
+        }
+        else {
+            throw new Error("No report generated yet");
+        }
+    }
+    //setter
+    set recentReport(currtReport) {
+        if (currtReport) {
+            this.currentReport = currtReport;
+        }
+        else {
+            throw new Error("wrong report");
+        }
+    }
+    addReport(report) {
+        this.reports.push(report);
+        this.currentReport = report;
+    }
+}
+const accountingFloor = new AccountingFloor("Neeraj", ["chetna", "namit"], []);
+console.log(accountingFloor);
+//call getter
+try {
+    console.log(accountingFloor.recentReport);
+}
+catch (error) {
+    console.log(error);
+}
+//call setter
+try {
+    accountingFloor.recentReport = "";
+}
+catch (error) {
+    console.log(error);
+}
+//call setter
+accountingFloor.recentReport = "Annual sales Report";
+//call getter
+console.log(accountingFloor.recentReport);
+accountingFloor.addReport("Budget");
+//call getter
+console.log(accountingFloor.recentReport);
+console.log(accountingFloor);
+//----------------------------- Getters & Setters Ends----------------
+//----------------------------- Static Methods & Properties----------------
+console.log("");
+console.log("------------- Static Methods & Properties-----------");
+let EmployeeUtils = /** @class */ (() => {
+    class EmployeeUtils {
+        static createEmployee(empName) {
+            return { name: empName };
+        }
+        static returnDouble(num) {
+            return num * 2;
+        }
+        returnHalf(num) {
+            return num / 2;
+        }
+        returnNextYear() {
+            //cant access static property from non static method using this
+            //return this.fiscalYear+1;
+            return EmployeeUtils.fiscalYear + 1;
+        }
+        static returnPreviousYear() {
+            //can access static property from  static method using this
+            return this.fiscalYear - 1;
+        }
+    }
+    EmployeeUtils.fiscalYear = 2020;
+    return EmployeeUtils;
+})();
+console.log(EmployeeUtils.fiscalYear);
+console.log(EmployeeUtils.returnDouble(22));
+console.log(EmployeeUtils.createEmployee("Rudhra"));
+//cant call non static method using classNa,e
+//console.log(EmployeeUtils.returnHalf(22));
+//static method call to return a static property
+console.log(EmployeeUtils.returnPreviousYear());
+//for normal method calls we need to create an object
+console.log(new EmployeeUtils().returnHalf(22));
+console.log(new EmployeeUtils().returnNextYear());
+//----------------------------- Static Methods & Properties Ends----------------
+//----------------------------- Abstract Classes----------------
+console.log("");
+console.log("------------- Abstract Classes -----------");
+//abstract class
+class Student {
+    constructor(name, rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+    //non abstract method
+    setStudentDetails(name, rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+}
+//cannot instantiate an abstract class
+//const student=new Student('Rudhra',22);
+class EngineeringStudent extends Student {
+    getStudentDetails() {
+        return new EngineeringStudent(this.name, this.rollNo);
+    }
+}
+const engineer = new EngineeringStudent("Rudhra", 22);
+console.log(engineer.getStudentDetails());
+//----------------------------- Abstract Classes Ends----------------
+//----------------------------- Singleton and Private constructors ----------------
+console.log("");
+console.log("------------- Singleton and Private constructors -----------");
+class SingletonDemo {
+    constructor(name) {
+        this.name = name;
+    }
+    static getInstance() {
+        if (this.instance) {
+            console.log("Instance available returning");
+            return this.instance;
+        }
+        console.log("no instance available creating a new instance and returning");
+        this.instance = new SingletonDemo("Rudhra");
+        return this.instance;
+    }
+}
+//cant instantiate using new keyword as constructor is private
+//const singleton = new SingletonDemo('Rudhra');
+const singletonClass = SingletonDemo.getInstance();
+console.log(singletonClass);
+console.log(SingletonDemo.getInstance());
+console.log(SingletonDemo.getInstance());
+//----------------------------- Singleton and Private constructors Ends----------------
+//----------------------------- Interfaces ----------------
+console.log("");
+console.log("------------- Interfaces -----------");
+const user1 = {
+    name: "Rudhra",
+    greet(message) {
+        console.log(message + "  " + this.name);
+    },
+};
+user1.greet("heyaa");
+//class implements interface
+class Greeter {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet(message) {
+        console.log(message + "  " + this.name);
+    }
+}
+//interface used as a type for a variable
+let Welcomer;
+//interface type variable saves an object of implementing class
+Welcomer = new Greeter("Rudhra", 30);
+console.log(Welcomer);
+Welcomer.greet("Good Morning ");
+//class implements interface
+class WishMan {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet(message) {
+        console.log(message + "  " + this.name);
+    }
+}
+let morningWisher;
+morningWisher = new WishMan("Rudhra", 30);
+morningWisher.greet("gud Morning");
+class AgedNightWisher {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    revealAge() {
+        return `the age of wisher is ${this.age}`;
+    }
+    greet(message) {
+        console.log(message + "  " + this.name);
+    }
+}
+const AgedNightWisherGuy = new AgedNightWisher("Avijit", 45);
+AgedNightWisherGuy.revealAge();
+AgedNightWisherGuy.greet("Gud night");
+let adder;
+//error as dder does not follow signature specified in AddFn
+//adder = (n1: string, n2: number) => n1 + n2;
+adder = (n1, n2) => n1 + n2;
+console.log(adder(2, 6));
+class EveningWisher {
+    //Constructor with optional properties
+    constructor(name, style, age, eyeColor = "Red", shirtColor) {
+        this.name = name;
+        this.style = style;
+        if (age) {
+            this.age = age;
+        }
+        if (eyeColor) {
+            this.eyeColor = eyeColor;
+        }
+        if (shirtColor) {
+            this.shirtColor = shirtColor;
+        }
+    }
+    revealAge() {
+        if (this.age) {
+            return `the age of wisher is ${this.age}`;
+        }
+        return "age is just a number";
+    }
+}
+const eveningWisher1 = new EveningWisher("Rudhra", "Retro");
+console.log(eveningWisher1);
+const eveningWisher2 = new EveningWisher("Chetna", "Jazz", 22);
+console.log(eveningWisher2);
+const eveningWisher3 = new EveningWisher("Vrinda", "Classical", 23, "Brown");
+console.log(eveningWisher3);
+const eveningWisher4 = new EveningWisher("Malay", "Cool", 24, "Black", "Blue");
+console.log(eveningWisher4);
+//cant just skip middle parameter
+//const eveningWisher5=new EveningWisher('Rudhra','Retro','Green');
 //# sourceMappingURL=app.js.map
--- Section5/src/app.ts
+++ Section5/src/app.ts
@@ -114,8 +114,9 @@ class BusinessUnit {
   private employees: string[];
 
   //constructor that initializes name property
-  constructor(n: string) {
+  constructor(n: string, employees: string[]) {
     this.name = n;
+    this.employees = employees;
   }
   public describe() {
     console.log("Welcome to " + this.name + " Business Unit");
@@ -145,7 +146,7 @@ class BusinessUnit {
   }
 }
 //initialize the BusinessUnit class using new keyword and constructor call
-const retailBu = new BusinessUnit("Retail");
+const retailBu = new BusinessUnit("Retail", []);
 
 console.log(retailBu);
 retailBu.describe();
@@ -291,3 +292,469 @@ console.log("sbuId is " + retailSBu.sbuId);
 retailSBu.describe();
 
 //-----------------------------Read only Properties Ends----------------
+
+//-----------------------------Inheritance----------------
+
+console.log("");
+console.log("-----------------------------Inheritance----------------");
+//class that extends another class
+class AccountingDepartment extends Project {}
+//parent class constructor is automatically called
+//we need to pass name argument as it is required by parent class constructor
+const accounting = new AccountingDepartment("Accounting");
+console.log(accounting);
+
+class ItDepartment extends Project {
+  private admins: string[];
+  constructor(name: string, admins: string[]) {
+    //calling constructor of base class should be first line
+    super(name);
+    this.admins = admins;
+  }
+}
+const itDepartment = new ItDepartment("It", ["hansaa"]);
+console.log(itDepartment);
+class AdminDepartment extends Project {
+  constructor(public admins: string[]) {
+    //calling constructor of base class should be first line
+    super("Admin");
+  }
+  //new methods added
+  addAdmin(admin: string) {
+    this.admins.push(admin);
+  }
+  displayAdmins() {
+    console.log(this.admins);
+  }
+}
+const AdminDept = new AdminDepartment(["Rudhra"]);
+console.log(AdminDept);
+
+AdminDept.addAdmin("karan");
+AdminDept.displayAdmins();
+AdminDept.describe();
+//-----------------------------Inheritance Ends----------------
+
+//-----------------------------Overriding Properties & The "protected" Modifier----------------
+
+console.log("");
+console.log(
+  "-------------Overriding Properties & The protected Modifier-----------"
+);
+
+class Floor {
+  constructor(
+    private floorName: string,
+    public floorMarshal: string,
+    protected employees: string[]
+  ) {}
+
+  private addEmployees(employee: string) {
+    this.employees.push(employee);
+  }
+
+  public printEmployees() {
+    console.log("the employees on the floor are " + this.employees);
+  }
+
+  protected validateAndAddEmployees(employee: string) {
+    if (employee) {
+      this.employees.push(employee);
+    }
+  }
+
+  protected currentFloorName() {
+    return this.floorName;
+  }
+}
+class AdminFloor extends Floor {
+  constructor(public floorMarshal: string, protected employees: string[]) {
+    super("FirstFloor", floorMarshal, employees);
+  }
+
+  /*
+  Cant override a private method
+  private addEmployees(employee: string) {
+    this.employees.push(employee);
+  } */
+
+  /* 
+  cant access private properties
+  public printEmployees() {
+    console.log("the employees on of "+this.floorName +' are ' +this.employees);
+  } */
+
+  //can override public methods
+  public printEmployees() {
+    //can access protected methods
+    console.log(
+      "the employees on of " +
+        this.currentFloorName() +
+        " are " +
+        this.employees
+    );
+  }
+  //can override protected method
+  protected validateAndAddEmployees(employee: string) {
+    if (employee !== "") {
+      //can access protected properties
+      this.employees.push(employee);
+    }
+  }
+
+  //override public method
+  public currentFloorName() {
+    //can access protected method
+    return super.currentFloorName();
+  }
+}
+
+const adminFloor = new AdminFloor("Neeraj", ["chetna", "namit"]);
+
+console.log(adminFloor);
+console.log(adminFloor.currentFloorName());
+//cant access protected members from outside the class
+//adminFloor.validateAndAddEmployees('shobhit');
+
+//-----------------------------Overriding Properties & The "protected" Modifier Ends----------------
+
+//----------------------------- Getters & Setters----------------
+
+console.log("");
+console.log("------------- Getters & Setters-----------");
+
+class AccountingFloor extends Floor {
+  private currentReport: string = "";
+
+  //getter
+  get recentReport() {
+    if (this.currentReport) {
+      return this.currentReport;
+    } else if (this.reports[0]) {
+      return this.reports[0];
+    } else {
+      throw new Error("No report generated yet");
+    }
+  }
+  //setter
+  set recentReport(currtReport: string) {
+    if (currtReport) {
+      this.currentReport = currtReport;
+    } else {
+      throw new Error("wrong report");
+    }
+  }
+  constructor(
+    public floorMarshal: string,
+    protected employees: string[],
+    public reports: string[]
+  ) {
+    super("FirstFloor", floorMarshal, employees);
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+    this.currentReport = report;
+  }
+}
+
+const accountingFloor = new AccountingFloor("Neeraj", ["chetna", "namit"], []);
+
+console.log(accountingFloor);
+//call getter
+try {
+  console.log(accountingFloor.recentReport);
+} catch (error) {
+  console.log(error);
+}
+//call setter
+try {
+  accountingFloor.recentReport = "";
+} catch (error) {
+  console.log(error);
+}
+//call setter
+accountingFloor.recentReport = "Annual sales Report";
+
+//call getter
+console.log(accountingFloor.recentReport);
+accountingFloor.addReport("Budget");
+
+//call getter
+console.log(accountingFloor.recentReport);
+
+console.log(accountingFloor);
+
+//----------------------------- Getters & Setters Ends----------------
+
+//----------------------------- Static Methods & Properties----------------
+
+console.log("");
+console.log("------------- Static Methods & Properties-----------");
+
+class EmployeeUtils {
+  static fiscalYear = 2020;
+
+  static createEmployee(empName: string) {
+    return { name: empName };
+  }
+
+  static returnDouble(num: number) {
+    return num * 2;
+  }
+
+  returnHalf(num: number) {
+    return num / 2;
+  }
+
+  returnNextYear() {
+    //cant access static property from non static method using this
+    //return this.fiscalYear+1;
+    return EmployeeUtils.fiscalYear + 1;
+  }
+  static returnPreviousYear() {
+    //can access static property from  static method using this
+    return this.fiscalYear - 1;
+  }
+}
+
+console.log(EmployeeUtils.fiscalYear);
+console.log(EmployeeUtils.returnDouble(22));
+console.log(EmployeeUtils.createEmployee("Rudhra"));
+//cant call non static method using classNa,e
+//console.log(EmployeeUtils.returnHalf(22));
+//static method call to return a static property
+console.log(EmployeeUtils.returnPreviousYear());
+//for normal method calls we need to create an object
+console.log(new EmployeeUtils().returnHalf(22));
+console.log(new EmployeeUtils().returnNextYear());
+//----------------------------- Static Methods & Properties Ends----------------
+
+//----------------------------- Abstract Classes----------------
+
+console.log("");
+console.log("------------- Abstract Classes -----------");
+
+//abstract class
+abstract class Student {
+  constructor(protected name: string, protected rollNo: number) {}
+
+  //abstract method
+  abstract getStudentDetails(): Student;
+  //non abstract method
+  setStudentDetails(name: string, rollNo: number) {
+    this.name = name;
+    this.rollNo = rollNo;
+  }
+}
+
+//cannot instantiate an abstract class
+//const student=new Student('Rudhra',22);
+
+class EngineeringStudent extends Student {
+  getStudentDetails(): Student {
+    return new EngineeringStudent(this.name, this.rollNo);
+  }
+}
+const engineer = new EngineeringStudent("Rudhra", 22);
+console.log(engineer.getStudentDetails());
+
+//----------------------------- Abstract Classes Ends----------------
+
+//----------------------------- Singleton and Private constructors ----------------
+
+console.log("");
+console.log("------------- Singleton and Private constructors -----------");
+
+class SingletonDemo {
+  private static instance: SingletonDemo;
+  private constructor(private name: string) {}
+
+  static getInstance() {
+    if (this.instance) {
+      console.log("Instance available returning");
+      return this.instance;
+    }
+    console.log("no instance available creating a new instance and returning");
+    this.instance = new SingletonDemo("Rudhra");
+    return this.instance;
+  }
+}
+//cant instantiate using new keyword as constructor is private
+//const singleton = new SingletonDemo('Rudhra');
+const singletonClass = SingletonDemo.getInstance();
+
+console.log(singletonClass);
+
+console.log(SingletonDemo.getInstance());
+
+console.log(SingletonDemo.getInstance());
+
+//----------------------------- Singleton and Private constructors Ends----------------
+
+//----------------------------- Interfaces ----------------
+
+console.log("");
+console.log("------------- Interfaces -----------");
+
+interface Greetable {
+  name: string;
+
+  greet(message: string): void;
+}
+
+const user1: Greetable = {
+  name: "Rudhra",
+
+  greet(message: string) {
+    console.log(message + "  " + this.name);
+  },
+};
+
+user1.greet("heyaa");
+
+//class implements interface
+class Greeter implements Greetable {
+  name: string;
+  //extra fields
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  greet(message: string): void {
+    console.log(message + "  " + this.name);
+  }
+}
+
+//interface used as a type for a variable
+let Welcomer: Greetable;
+//interface type variable saves an object of implementing class
+Welcomer = new Greeter("Rudhra", 30);
+console.log(Welcomer);
+Welcomer.greet("Good Morning ");
+
+interface Wisher {
+  readonly name: string;
+
+  greet(message: string): void;
+}
+
+//class implements interface
+class WishMan implements Wisher {
+  name: string;
+  //extra fields
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  greet(message: string): void {
+    console.log(message + "  " + this.name);
+  }
+}
+
+let morningWisher: Wisher;
+morningWisher = new WishMan("Rudhra", 30);
+morningWisher.greet("gud Morning");
+//read only beaviour enforced because of interface
+//morningWisher.name='Vikas';
+
+//Interface inheritance
+interface AgedWishMan extends Wisher {
+  age: number;
+  revealAge(): string;
+}
+
+class AgedNightWisher implements AgedWishMan {
+  age: number;
+  name: string;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  revealAge(): string {
+    return `the age of wisher is ${this.age}`;
+  }
+
+  greet(message: string): void {
+    console.log(message + "  " + this.name);
+  }
+}
+
+const AgedNightWisherGuy = new AgedNightWisher("Avijit", 45);
+AgedNightWisherGuy.revealAge();
+AgedNightWisherGuy.greet("Gud night");
+
+//Interface as a function type
+interface AddFn {
+  (n1: number, n2: number): number;
+}
+let adder: AddFn;
+//error as dder does not follow signature specified in AddFn
+//adder = (n1: string, n2: number) => n1 + n2;
+adder = (n1: number, n2: number) => n1 + n2;
+console.log(adder(2, 6));
+
+//Interface with optional properties
+interface StylishWishMan {
+  //Optional property
+  age?: number;
+  readonly name: string;
+  //Optional property
+  style?: string;
+  //Optional property
+  eyeColor?: string;
+  //Optional method
+  greet?(message: string): void;
+  revealAge(): string;
+}
+
+class EveningWisher implements StylishWishMan {
+  //Optional property in interface and class
+  age?: number;
+  name: string;
+  //Optional property in interface required in class
+  style: string;
+  //Optional property in interface and class
+  eyeColor?: string;
+  //Optional property in interface and class
+  shirtColor?: string;
+  //Constructor with optional properties
+  constructor(
+    name: string,
+    style: string,
+    age?: number,
+    eyeColor: string = "Red",
+    shirtColor?: string
+  ) {
+    this.name = name;
+    this.style = style;
+    if (age) {
+      this.age = age;
+    }
+    if (eyeColor) {
+      this.eyeColor = eyeColor;
+    }
+    if (shirtColor) {
+      this.shirtColor = shirtColor;
+    }
+  }
+  revealAge(): string {
+    if (this.age) {
+      return `the age of wisher is ${this.age}`;
+    }
+    return "age is just a number";
+  }
+}
+
+const eveningWisher1 = new EveningWisher("Rudhra", "Retro");
+console.log(eveningWisher1);
+const eveningWisher2 = new EveningWisher("Chetna", "Jazz", 22);
+console.log(eveningWisher2);
+const eveningWisher3 = new EveningWisher("Vrinda", "Classical", 23, "Brown");
+console.log(eveningWisher3);
+const eveningWisher4 = new EveningWisher("Malay", "Cool", 24, "Black", "Blue");
+console.log(eveningWisher4);
+//cant just skip middle parameter
+//const eveningWisher5=new EveningWisher('Rudhra','Retro','Green');
