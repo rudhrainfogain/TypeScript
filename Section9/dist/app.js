706d7ec1429acb4ce2f365448c3888572b478d13
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-10-13 00:42:06 +0530

    Step 2- Interacting with DOM Elements

--- Section9/dist/app.js
+++ Section9/dist/app.js
@@ -8,12 +8,28 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         //access the form
         this.element = importedNode.firstElementChild;
+        //Add style to form
+        this.element.id = 'user-input';
+        //get access to input elements
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        //handle submit event
+        this.configure();
         //add element to dom
         this.attach();
     }
     attach() {
         //add element to div
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        //handle submit event of form
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 }
 const prjInput = new ProjectInput();
--- Section9/src/app.ts
+++ Section9/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
   constructor() {
     //get access to template and div
     this.templateElement = document.getElementById(
@@ -16,6 +19,22 @@ class ProjectInput {
     );
     //access the form
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    //Add style to form
+    this.element.id = 'user-input';
+
+    //get access to input elements
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    //handle submit event
+    this.configure();
     //add element to dom
     this.attach();
   }
@@ -23,6 +42,14 @@ class ProjectInput {
   private attach() {
     //add element to div
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    //handle submit event of form
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 }
 const prjInput = new ProjectInput();
