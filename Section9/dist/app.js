37016da77e71f340409093fe1217d071726db97d
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-10-13 00:19:47 +0530

    step -1 DOM Element Selection & OOP Rendering

--- Section9/dist/app.js
+++ Section9/dist/app.js
@@ -1,3 +1,20 @@
 "use strict";
-console.log('Practiceee');
+class ProjectInput {
+    constructor() {
+        //get access to template and div
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        //import content from template
+        const importedNode = document.importNode(this.templateElement.content, true);
+        //access the form
+        this.element = importedNode.firstElementChild;
+        //add element to dom
+        this.attach();
+    }
+    attach() {
+        //add element to div
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- Section9/src/app.ts
+++ Section9/src/app.ts
@@ -1,1 +1,28 @@
-console.log('Practiceee');
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    //get access to template and div
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    //import content from template
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    //access the form
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    //add element to dom
+    this.attach();
+  }
+
+  private attach() {
+    //add element to div
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+const prjInput = new ProjectInput();
