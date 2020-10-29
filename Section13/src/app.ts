41ae1a2a35ef5103015c59388bad67743b670241
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2020-10-30 03:00:10 +0530

    Getting user input

--- Section13/src/app.ts
+++ Section13/src/app.ts
@@ -1,3 +1,14 @@
-console.log(
-  'A strong Mind looks right in the eyes of a problem and says vanish!!!!'
-);
+//get access to  form
+const form = document.querySelector('form')!;
+//get access to input box
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+//  search handler function
+function searchAddressHandler(event: Event) {
+  //prevent default behaviour of form submission
+  event.preventDefault();
+  //get value eneterd by user in address input  box
+  const enteredAddress = addressInput.value;
+}
+//add event listener to form to listen to the submit event
+form.addEventListener('submit', searchAddressHandler);
